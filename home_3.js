69dba05da9ee1dd1562e41c83590520c93df0357
Melnyk Yevhenii <karimovzena457@gmail.com>
2024-08-19 11:15:52 +0300

    homework completed

--- home_3.js
+++ home_3.js
@@ -1,6 +1,12 @@
-function camelCase(str = " make it   happen ") {
-    str = str.trim();
-  
+function camelCase(str, separator) {
+  const parts = str.split(separator);
+  let camelCased = parts[0].toLowerCase();
+  for (let i = 1; i < parts.length; i++) {
+      let word = parts[i];
+      let capitalizedWord = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+      camelCased += capitalizedWord;
+  }
+  return camelCased;
 }
-
-  console.log(camelCase())
+const result = camelCase("hello_world_example", "_");
+console.log(result);
